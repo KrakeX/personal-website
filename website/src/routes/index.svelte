e3a191505377a139992d5548bdb60eb914dc88c8
KrakeX <duran.pablo.c@gmail.com>
2022-06-29 17:54:01 -0400

    upload changes

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,37 +1,37 @@
 <script context="module">
-    export async function load({params, fetch}){
-        const res = await fetch(`/api/jobs`);
-        const data = await res.json();
-    return {props: {jobs: data}}
-    }
+	export async function load({ params, fetch }) {
+		const res = await fetch(`/api/jobs`);
+		const data = await res.json();
+		return { props: { jobs: data } };
+	}
 </script>
 
 <script>
-    
-    import JobCard from '../components/jobs/job-card.svelte';
+	import JobCard from '../components/jobs/job-card.svelte';
 
-    export let jobs;
+	export let jobs;
 
-    let searchTerm = "";
-    let filteredJobs = [];
+	let searchTerm = '';
+	let filteredJobs = [];
 
-    $:{
-        if(searchTerm){
-            filteredJobs = jobs.filter(job => {
-                let scopedSearch = searchTerm.toLowerCase();
-                const tags = job.tags.toString().toLowerCase();
-                const termsToSearch = scopedSearch.replace(',',' ').split(' ');
-                for(const term of termsToSearch){
-                    return tags.match(new RegExp("\\b"+term+"\\b", "i")) != null;
-                }
-            })
-        }else{
-            filteredJobs = [...jobs]
-        }
-    }
+	$: {
+		if (searchTerm) {
+			filteredJobs = jobs.filter((job) => {
+				let scopedSearch = searchTerm.toLowerCase();
+				const tags = job.tags.toString().toLowerCase();
+				const termsToSearch = scopedSearch.replace(',', ' ').split(' ');
+				for (const term of termsToSearch) {
+					return tags.match(new RegExp('\\b' + term + '\\b', 'i')) != null;
+				}
+			});
+		} else {
+			filteredJobs = [...jobs];
+		}
+	}
 </script>
+
 <svelte:head>
-    <title>Pablo Durán - Webpage</title>
+	<title>Pablo Durán - Webpage</title>
 </svelte:head>
 
 <h1 class="text-4xl text-center my-8 uppercase">Welcome to Pablo Durán's Webpage</h1>
@@ -39,10 +39,7 @@
 <!-- <input class="w-full rounder-md text-lg p-4 border-2 border-gray-200" type="text" bind:value="{searchTerm}" placeholder="What experience do you need from me?"> -->
 
 <div class="grid gap-4 md:grid-cols-2 grid-cols-1">
-    {#each jobs as job}
-        <JobCard job={job} />
-    {/each}
+	{#each jobs as job}
+		<JobCard {job} />
+	{/each}
 </div>
-
-
-
--- website/src/routes/job/[id].svelte
+++ website/src/routes/job/[id].svelte
@@ -1,14 +1,16 @@
 <script context="module">
-    export async function load({params, fetch}){
-        const res = await fetch(`/api/jobs/${+params.id}`)
+	export async function load({ params, fetch }) {
+		const res = await fetch(`/api/jobs/${+params.id}`);
 		const job = await res.json();
-		console.log(job)
-        return { props: { job }}
-    }
+		console.log(job);
+		return { props: { job } };
+	}
 </script>
+
 <script>
-    export let job;
+	export let job;
 </script>
+
 <div class="flex flex-col items-center">
-    <h1 class="text-4xl text-center my-8 uppercase" >{job.title}</h1>
+	<h1 class="text-4xl text-center my-8 uppercase">{job.title}</h1>
 </div>
