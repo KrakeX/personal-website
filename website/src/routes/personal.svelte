344b08ad3cb84fde3d8372713cc02d359882017d
Pablo Durán Celis <pablo.durancelis@cencosud.cl>
2022-07-03 02:57:40 -0400

    work in progress message updated

--- website/src/routes/personal.svelte
+++ website/src/routes/personal.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
     <title>About Pablo Durán</title>
 </svelte:head>
-<h1 class="text-4xl text-center my-8 uppercase">This will be where we will find out how awesome is Pablo</h1>
+<h1 class="text-4xl text-center my-8 uppercase">Soon you will know more about me...</h1>
