edfe3af9978cc6ef889daf8d34e3eaffb15d4ce3
KrakeX <duran.pablo.c@gmail.com>
2022-07-05 00:38:35 -0400

    added footer and background gradients

--- website/src/routes/personal.svelte
+++ website/src/routes/personal.svelte
@@ -1,4 +1,4 @@
 <svelte:head>
-    <title>About Pablo Durán</title>
+	<title>About Pablo - Pablo Durán Celis</title>
 </svelte:head>
 <h1 class="text-4xl text-center my-8 uppercase">Soon you will know more about me...</h1>
